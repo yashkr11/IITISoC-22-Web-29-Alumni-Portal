bfcbf165ed966a7e1ef83b72ce33600f1d2c97b7
yashkr11 <106267816+yashkr11@users.noreply.github.com>
2022-08-14 11:44:30 +0530

    Add files via upload

--- hello/home/urls.py
+++ hello/home/urls.py
@@ -4,6 +4,7 @@ from home import views
 
 urlpatterns = [
     path("",views.index, name='home'),
+    path("index",views.index, name='index'),
     path("about",views.about, name='about'),
     path("services",views.services, name='services'),
     path("contact",views.contact, name='contact'),
